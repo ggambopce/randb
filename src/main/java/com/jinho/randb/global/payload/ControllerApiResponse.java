08d4baa9a00c455de429c466fa4b9fc0c93b28c9
ggambopce <ggambopce@gmail.com>
2024-09-25 21:45:29 +0900

    컨트롤러 응답메세지 구현

---
+++ src/main/java/com/jinho/randb/global/payload/ControllerApiResponse.java
@@ -0,0 +1,34 @@
+package com.jinho.randb.global.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class ControllerApiResponse<T> {
+
+    @Schema(example = "true")
+    private boolean success;
+
+    @Schema(description = "응답 메세지")
+    private String message;
+
+    @Schema(description = "응답 데이터 컬렉션 형태")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public ControllerApiResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public ControllerApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
