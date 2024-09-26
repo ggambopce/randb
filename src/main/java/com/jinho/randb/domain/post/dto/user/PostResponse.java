04c1b7402c734f8d0e8196392a82a05b40cbba8b
ggambopce <ggambopce@gmail.com>
2024-09-26 14:00:33 +0900

    조회기능 구현

---
+++ src/main/java/com/jinho/randb/domain/post/dto/user/PostResponse.java
@@ -0,0 +1,13 @@
+package com.jinho.randb.domain.post.dto.user;
+
+import com.jinho.randb.domain.post.dto.PostDto;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PostResponse {
+
+    }
+
+}
