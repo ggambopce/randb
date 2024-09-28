b374847a647cbee4dda5df2c62246c39aaa83ee9
ggambopce <ggambopce@gmail.com>
2024-09-28 17:07:19 +0900

    게시판 CRUD 구현

--- src/main/java/com/jinho/randb/domain/post/application/PostService.java
+++ src/main/java/com/jinho/randb/domain/post/application/PostService.java
@@ -1,9 +1,24 @@
 package com.jinho.randb.domain.post.application;
 
+import com.jinho.randb.domain.post.domain.Post;
+import com.jinho.randb.domain.post.dto.user.PostDetailResponse;
+import com.jinho.randb.domain.post.dto.user.PostResponse;
 import com.jinho.randb.domain.post.dto.user.UserAddRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
 
     void save(UserAddRequest userAddRequest);
+
+    Optional<Post> findById(Long id);
+
+    List<Post> findAll();
+
+    void delete(Long postId);
+
+
 
 }
