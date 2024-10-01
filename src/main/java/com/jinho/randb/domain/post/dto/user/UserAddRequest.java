2df75e5017c51061f80ba8c8d8a83ceceac9b348
ggambopce <ggambopce@gmail.com>
2024-10-01 12:31:39 +0900

    PostServiceImpl TEST 구현

--- src/main/java/com/jinho/randb/domain/post/dto/user/UserAddRequest.java
+++ src/main/java/com/jinho/randb/domain/post/dto/user/UserAddRequest.java
@@ -2,11 +2,13 @@ package com.jinho.randb.domain.post.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserAddRequest {
 
     @NotEmpty(message = "토론 제목을 입력해주세요")
