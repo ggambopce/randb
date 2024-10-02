6c68ef839a766829f733af66b8331c8468d9c767
ggambopce <ggambopce@gmail.com>
2024-10-02 15:07:58 +0900

    Post게시판Swagger 작성

--- src/main/java/com/jinho/randb/domain/post/api/PostController.java
+++ src/main/java/com/jinho/randb/domain/post/api/PostController.java
@@ -50,35 +50,63 @@ public class PostController {
         return ResponseEntity.ok(new ControllerApiResponse(true, "작성 성공"));
     }
 
-    /*
-    토론게시글 목록조회
-     */
+    @Operation(summary = "전체 토론글 조회 API", description = "토론글의 전체 목록을 조회할 수 있습니다.", tags = {"일반 사용자 토론글 컨트롤러"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = Post.class),
+                            examples = @ExampleObject(value = "{\"success\": true, \"message\" : \"조회 성공\",\"posts\":[{\"id\":23, \"postTitle\" : \"새로운 토론 주제\",\"postContent\" : \"이것은 토론의 내용입니다.\"}]}")))
+    })
     @GetMapping("/api/posts")
     public ResponseEntity<?> findAllPost() {
         List<Post> posts = postService.findAll();
         return ResponseEntity.ok(new ControllerApiResponse<>(true, "조회성공", posts));
     }
 
-    /*
-    토론게시글 상세조회
-     */
+    @Operation(summary = "토론글 상세 조회 API", description = "토론글의 상세 정보를 조회할 수 있습니다.", tags = {"일반 사용자 토론글 컨트롤러"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = Post.class),
+                            examples = @ExampleObject(value = "{\"success\":true,\"message\":\"조회성공\",\"data\":{\"post\":{\"id\":3,\"postTitle\":\"토론 주제\",\"postContent\":\"토론 내용입니다!\"}}}"))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            examples =  @ExampleObject(value = "{\"success\": false, \"message\": \"해당하는 게시물이 없습니다.\"}")))
+    })
     @GetMapping("/api/user/posts/{post-id}")
     public ResponseEntity<?> findPost(@PathVariable("post-id") long id) {
         Optional<Post> post = postService.findById(id);
         return ResponseEntity.ok(new ControllerApiResponse<>(true, "조회성공", post));
     }
-    /*
-    토론게시글 삭제
-     */
+
+    @Operation(summary = "토론글 삭제 API",description = "삭제시 해당 게시물과 관련된 데이터는 모두 삭제",tags = {"일반 사용자 토론글 컨트롤러"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = ControllerApiResponse.class),
+                            examples = @ExampleObject(value = "{\"success\": true, \"message\" : \"토론글 삭제 성공\"}"))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"success\": false, \"message\" : \"게시글을 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"success\": false, \"message\" : \"작성자만 삭제할수 있습니다.\"}")))
+    })
     @DeleteMapping("/api/user/posts/{post-id}")
     public ResponseEntity<?> deletePost(@PathVariable("post-id") Long postId){
         postService.delete(postId);
         return ResponseEntity.ok(new ControllerApiResponse<>(true, "게시글 삭제 성공"));
     }
 
-    /*
-    토론게시글 수정
-    */
+    @Operation(summary = "토론글 수정 API", description = "로그인한 사용자만 수정이 가능하며 작성자만 수정이 가능하도록 이전에 비밀번호 검증을 통해서 검증확인해 해당 API 접근가능", tags = {"일반 사용자 토론글 컨트롤러"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = ControllerApiResponse.class),
+                            examples = @ExampleObject(value = "{\"success\": true, \"message\": \"토론글 수정 성공\"}"))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"success\": false, \"message\" : \"게시글을 찾을수 없습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"success\": false, \"message\" : \"작성자만 수정할 수 있습니다.\"}")))
+    })
     @PostMapping("/api/user/update/posts/{post-id}")
     public ResponseEntity<?> updatePost(@Valid @RequestBody UserUpdateRequest updatePostDto, @PathVariable("post-id") Long postId){
         postService.update(postId, updatePostDto);
