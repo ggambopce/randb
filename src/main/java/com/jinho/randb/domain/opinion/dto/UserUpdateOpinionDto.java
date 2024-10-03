d4b7e20c48d2b567d9c1e19bb281abead02a1279
ggambopce <ggambopce@gmail.com>
2024-10-03 09:48:26 +0900

    의견 테스트 코드 작성

--- src/main/java/com/jinho/randb/domain/opinion/dto/UserUpdateOpinionDto.java
+++ src/main/java/com/jinho/randb/domain/opinion/dto/UserUpdateOpinionDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @NoArgsConstructor
 public class UserUpdateOpinionDto {
 
--- src/main/java/com/jinho/randb/domain/post/dto/user/UserUpdateRequest.java
+++ src/main/java/com/jinho/randb/domain/post/dto/user/UserUpdateRequest.java
@@ -3,10 +3,12 @@ package com.jinho.randb.domain.post.dto.user;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserUpdateRequest {
