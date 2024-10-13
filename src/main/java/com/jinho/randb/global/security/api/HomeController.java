94d64033c3e69457de8f7c9ca0727fbbf846e96c
ggambopce <ggambopce@gmail.com>
2024-10-13 12:44:59 +0900

    시큐리티 설정

---
+++ src/main/java/com/jinho/randb/global/security/api/HomeController.java
@@ -0,0 +1,25 @@
+package com.jinho.randb.global.security.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping(value="/")
+    public String dashboard() {
+        return "/dashboard";
+    }
+
+    @GetMapping(value="/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping(value="/manager")
+    public String manager() {
+        return "/manager";
+    }
+
+    @GetMapping(value="/admin")
+    public String admin() {
+        return "/admin";
+    }
+}
