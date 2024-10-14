018b114712d50dd4752ff36fd7f19b564ebee2ca
ggambopce <ggambopce@gmail.com>
2024-10-14 22:02:35 +0900

    로그아웃 구현

--- src/main/java/com/jinho/randb/domain/account/api/LoginController.java
+++ src/main/java/com/jinho/randb/domain/account/api/LoginController.java
@@ -1,5 +1,11 @@
 package com.jinho.randb.domain.account.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,5 +22,14 @@ public class LoginController {
     public String signup() {
 
         return "login/signup";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+        return  "redirect:/login";
     }
 }
