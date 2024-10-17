d53b70980ba73ede7da8f3c86d5fe4ab37693bed
ggambopce <ggambopce@gmail.com>
2024-10-17 10:46:58 +0900

    csrf토큰 설정

--- src/main/java/com/jinho/randb/global/security/config/SecurityConfig.java
+++ src/main/java/com/jinho/randb/global/security/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/manager").hasAuthority("ROLE_MANAGER")
                         .requestMatchers("/api/admin").hasAuthority("ROLE_ADMIN")
                         .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(restAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
                 .authenticationManager(authenticationManager)
                 .exceptionHandling(exception -> exception
