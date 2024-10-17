3dc5ce1d57af8cf0d072fdf9f00d042a08d7f8fa
ggambopce <ggambopce@gmail.com>
2024-10-17 10:40:40 +0900

    권한설정 완료

--- src/main/java/com/jinho/randb/global/security/config/SecurityConfig.java
+++ src/main/java/com/jinho/randb/global/security/config/SecurityConfig.java
@@ -79,6 +79,9 @@ public class SecurityConfig {
                 .securityMatcher("/api/**")
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
+                        .requestMatchers("/api/user").hasAuthority("ROLE_USER")
+                        .requestMatchers("/api/manager").hasAuthority("ROLE_MANAGER")
+                        .requestMatchers("/api/admin").hasAuthority("ROLE_ADMIN")
                         .anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(restAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
