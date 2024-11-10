5e53784c85bdf327a036635142d0fcc245dbfd16
ggambopce <ggambopce@gmail.com>
2024-11-10 18:08:39 +0900

    JWT 시큐리티 필터체인 설정

--- src/main/java/com/jinho/randb/global/security/config/SecurityConfig.java
+++ src/main/java/com/jinho/randb/global/security/config/SecurityConfig.java
@@ -1,26 +1,13 @@
 package com.jinho.randb.global.security.config;
 
-import com.jinho.randb.global.security.entrypoint.RestAuthenticationEntryPoint;
-import com.jinho.randb.global.security.filter.RestAuthenticationFilter;
-import com.jinho.randb.global.security.handler.*;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationDetailsSource;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import org.springframework.security.web.context.DelegatingSecurityContextRepository;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 
 
 @Configuration
@@ -28,94 +15,30 @@ import org.springframework.security.web.context.RequestAttributeSecurityContextR
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationProvider authenticationProvider;
-    private final AuthenticationProvider restAuthenticationProvider;
-    private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
-    private final FormAuthenticationSuccessHandler successHandler;
-    private final FormAuthenticationFailureHandler failureHandler;
-    private final RestAuthenticationSuccessHandler restSuccessHandler;
-    private final RestAuthenticationFailureHandler restFailureHandler;
-
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(restAuthenticationProvider);
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        //csrf disable
         http
-
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*","/swagger-ui/**", "/api-docs/**").permitAll()
-                        .requestMatchers("/signup","/login*", "/").permitAll()
-                        .requestMatchers("/main").permitAll()
-                        .requestMatchers("/user").hasAuthority("ROLE_USER")
-                        .requestMatchers("/manager").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated())
-
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
-                        .authenticationDetailsSource(authenticationDetailsSource)
-                        .successHandler(successHandler)
-                        .failureHandler(failureHandler)
-                        .permitAll())
-
-                .authenticationProvider(authenticationProvider)
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(new FormAccessDeniedHandler("/denied")))
-                ;
-
+                .csrf((auth) -> auth.disable());
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+        //oauth2
+        http
+                .oauth2Login(Customizer.withDefaults());
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(restAuthenticationProvider);
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();            // build() 는 최초 한번 만 호출해야 한다
-
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*","/swagger-ui/**","/api-docs/**").permitAll()
-                        .requestMatchers("/signup","api/login*","rest/main","/").permitAll()// /main 경로는 로그인 없이 접근 허용
-                        .requestMatchers("/api/admin/**").permitAll()
-                        .requestMatchers("/main","/posts", "/signup").permitAll() // /main 경로는 모두 접근 가능
-                        .requestMatchers(HttpMethod.POST,"/api/join").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/user/posts").hasAuthority("ROLE_USER")  // 게시물 작성은 ROLE_USER 권한 필요
-                        .requestMatchers(HttpMethod.POST, "/api/user/opinions").hasAuthority("ROLE_USER") //의견작성은 ROLE_USER 권한 필요
-                        .requestMatchers("/api/user").hasAuthority("ROLE_USER")
-                        .requestMatchers("/api/manager").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/api/admin").hasAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated()
-                )
-
-                //.csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(restAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
-                .authenticationManager(authenticationManager)
-                .exceptionHandling(exception -> exception
-                    .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-                    .accessDeniedHandler(new RestAccessDeniedHandler())
-                )
-        ;
-        return http.build();
-    }
-
-    private RestAuthenticationFilter restAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
-
-        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter(http);
-        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
-        restAuthenticationFilter.setAuthenticationSuccessHandler(restSuccessHandler);
-        restAuthenticationFilter.setAuthenticationFailureHandler(restFailureHandler);
-        restAuthenticationFilter.setSecurityContextRepository(new DelegatingSecurityContextRepository(
-                new RequestAttributeSecurityContextRepository(), new HttpSessionSecurityContextRepository()));
-
-        return restAuthenticationFilter;
-    }
 }
